5443bb47e9d106cd8d7abeb363408108bc875eba
Jared Kohrt <jkohrt7@gmail.com>
2022-02-14 12:05:57 -0500

    mobile layout for about page

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
       <Hero/>
 
       <main className = "min-h-screen bg-neutral-900 flex flex-col">
-        <div className = "flex space-x-16 align-middle justify-center mt-8 p-6 mb-6 border-b-4 border-double m-auto">
+        <div className = "hidden md:flex space-x-16 align-middle justify-center mt-8 p-6 mb-6 border-b-4 border-double m-auto ">
             <NavLink to = "/" className = {({isActive}) => (isActive ? "text-2xl secondary-nav-active": "text-2xl secondary-nav")}>About</NavLink>
             <NavLink to = "/crew" className = {({isActive}) => (isActive ? "text-2xl secondary-nav-active": "text-2xl secondary-nav")}>Crew</NavLink>
             <NavLink to = "/calendar" className = {({isActive}) => (isActive ? "text-2xl secondary-nav-active": "text-2xl secondary-nav")}>Calendar</NavLink>
@@ -46,7 +46,7 @@ function App() {
             >
               
               <Routes location = {location}>
-                <Route path = "/" element = {<About/>} exact />
+                <Route path = "/Custard_Crew" element = {<About/>} exact />
                 <Route path = "/crew" element = {<Crew/>}/>
                 <Route path = "/calendar" element = {<Calendar/>}/>
               </Routes>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
         <header>
             <div className = "Hero justify-center p-12">
                 <div className='flex flex-col'>
-                    <div className = "text-white text-4xl text-center max-w-sm leading-2">
+                    <div className = "text-white text-4xl text-center md:max-w-sm leading-2">
                         Welcome to the home of the Custard Crew.
                     </div>
                     <div className='border-b-2 border-white w-48 self-center py-4'></div>
@@ -20,7 +20,6 @@ function Hero() {
                         <img className = 'w-12 pt-4' src = {TwitchLogo}></img>
                     </a>
                     
-
                 </div>
 
             </div>
