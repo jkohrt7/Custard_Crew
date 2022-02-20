bc06f0a531ffb03d1ee7fa5178d6ecf6930ec559
Jared Kohrt <jkohrt7@gmail.com>
2022-02-20 17:20:40 -0500

    File reorganization for deployment

---
+++ client/src/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+import './SecondaryNav.css';
+import './fonts.css';
+
+import { Routes, Route, Link, NavLink, isActive, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './routes/About';
+import Crew from './routes/Crew';
+import Calendar from './routes/Calendar';
+import Footer from './components/Footer';
+import Hamburger from './components/Hamburger';
+
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+function App() {
+  let buttonFormat = "text-white text-lg px-4 w-24 transition ease-in-out duration-75 hover:bg-gray-600 m-auto text-center p-6 m-0" ;
+  const location = useLocation();
+
+  return (
+    <div>
+      <Navbar>
+        <Hamburger/>
+      </Navbar>
+
+      <Hero/>
+
+      <main className = "min-h-screen bg-neutral-900 flex flex-col">
+        <div className = "hidden md:flex space-x-16 align-middle justify-center mt-8 p-6 mb-6 border-b-4 border-double m-auto ">
+            <NavLink to = "/" className = {({isActive}) => (isActive ? "text-2xl secondary-nav-active": "text-2xl secondary-nav")}>About</NavLink>
+            <NavLink to = "/crew" className = {({isActive}) => (isActive ? "text-2xl secondary-nav-active": "text-2xl secondary-nav")}>Crew</NavLink>
+            <NavLink to = "/calendar" className = {({isActive}) => (isActive ? "text-2xl secondary-nav-active": "text-2xl secondary-nav")}>Calendar</NavLink>
+        </div>
+
+        <div id = "router">
+          <TransitionGroup>
+            <CSSTransition
+              key = {location.pathname}
+              classNames = "fade"
+              timeout={200}
+            >
+              
+              <Routes location = {location}>
+                <Route path = "/" element = {<About/>} exact />
+                <Route path = "/crew" element = {<Crew/>}/>
+                <Route path = "/calendar" element = {<Calendar/>}/>
+              </Routes>
+
+            </CSSTransition>
+          </TransitionGroup>
+        </div>
+
+
+      </main>
+
+      <footer>
+      
+        <Footer/>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/routes/About.js
@@ -0,0 +1,38 @@
+import placeholder from '../img/placeholder.jpg';
+import WBTW from '../img/WBTW.jpg';
+import {AboutUs} from '../text_files.js';
+import tempCrew from '../img/crew.jpg';
+
+function About() {
+    return(
+        <section className = "flex flex-col p-8 max-w-6xl mx-auto space-y-2">
+            <div id = "About-us__sub-header" className = "text-yellow-600 text-3xl">
+                About Us 
+            </div>
+            <div id = "About-us__header" className = "text-white text-4xl">
+                A DnD Group from NC
+            </div>
+            <div className = "text-white text-lg leading-8 pt-4">
+                {AboutUs.MainAboutUs}
+            </div>
+            <div className = "flex flex-col lg:flex-row justify-center align-middle pt-2 md:pt-10 m-auto">
+                <img className = " border-yellow-600 border-4 md:max-w-sm m-2 md:m-4 aspect-auto" src = {tempCrew}></img>
+            </div>
+            <div id = "About-us__sub-header" className = "text-yellow-600 text-3xl pt-16">
+                Current Campaign
+            </div>
+            <div id = "About-us__header" className = "text-white text-4xl">
+                The Wild Beyond the Witchlight
+            </div>
+            <div className = "text-white text-lg leading-8 pt-4">
+                Once every eight years, the fantastic Witchlight Carnival touches down on your world, bringing joy to one settlement after the next. Its owners, Mister Witch and Mister Light, know how to put on a good show. But there’s more to this magical extravaganza than meets the eye!
+            </div>
+            <div className = "flex justify-center space-x-4 pt-10">
+                <img className = "border-yellow-600 border-4 md:max-w-lg m-2 md:m-4" src = {WBTW}></img>
+            </div>
+
+        </section>
+    );
+}
+
+export default About;
